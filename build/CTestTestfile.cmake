9c6c6e1e0394f686f2e5986cb90cecbddb5be3bc
Aytekin E <aaytekinerdogan@gmail.com>
2022-07-03 02:20:40 +0300

    gitignored

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ae/repo/ComputerVisionCuda
-# Build directory: /home/ae/repo/ComputerVisionCuda/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
